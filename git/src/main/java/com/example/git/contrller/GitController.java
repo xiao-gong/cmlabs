f173dfbe783d2039c1c77016c5dd106b661d352e
xiao-gong <15114807610@163com@>
2020-07-14 23:24:24 +0800

    createGit

---
+++ git/src/main/java/com/example/git/contrller/GitController.java
@@ -0,0 +1,23 @@
+package com.example.git.contrller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * todo:测试 git
+ * @author xiaogong
+ * @date  20200714
+ */
+@RestController("/git")
+public class GitController {
+
+    @RequestMapping("/getGit")
+    public String getGit(){
+        return "git";
+    }
+
+    @RequestMapping
+    public void delGit(){
+        return;
+    }
+}
---
+++ git/src/test/java/com/example/git/GitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
